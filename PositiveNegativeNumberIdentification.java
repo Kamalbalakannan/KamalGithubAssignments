becf15175d6f9b42211ac2a56695a2f44ee4625c
Kamalbalakannan <90106023+Kamalbalakannan@users.noreply.github.com>
2021-09-04 23:18:46 +0530

    Add files via upload

---
+++ PositiveNegativeNumberIdentification.java
@@ -0,0 +1,17 @@
+package firstweek.day1;
+
+public class PositiveNegativeNumberIdentification {
+
+	public static void main(String[] args) {
+		int number = -25;
+		int number1 = 35;
+		if (number<0) {
+			System.out.println("The number " + number + " is Negative");
+		}
+		if (number1>0) {
+		 System.out.println("The number " + number1 + " is Positive");
+	}
+		else
+			System.out.println("The number is neither Negative nor Positive");
+	}
+}
